b35e16d01fc45e4e9297e5484a6092659cb594f0
esther-splett <esthersplett@gmail.com>
2019-03-12 16:35:33 +0000

    added an endpoint/link that displays html

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 //endpoints
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
